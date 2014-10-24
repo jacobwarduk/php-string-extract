4f5bc2bee724ac2e09b086d7ebd3a784687a09b8
Jacob Ward <jacobward@Jacobs-MacBook-Pro.local>
2014-10-24 20:09:49 +0100

    Updated to return false if either of entered strings are not found.

--- stringExtract.php
+++ stringExtract.php
@@ -1,10 +1,13 @@
 <?php
-    // Function for extracting a string, given a start and end point
-    function stringExtract($item, $start, $end){
-        $item = stristr($item, $start);
-    	$item = substr($item, strlen($start));
-    	$stop = stripos($item, $end);
-    	$data = substr($item, 0, $stop);
-    	return $data;
+    // Function for extracting a string from within a string, given a start and end point
+    function stringExtract($item, $start, $end) {
+    	if (($startPos = stripos($item, $start)) === false) {	// If $start string is not found
+    		return false;	// Return false
+    	} else if (($endPos = stripos($item, $end)) === false) {	// If $end string is not found
+    		return false;	// Return false
+    	} else {
+    		$substrStart = $startPos + strlen($start);	// Assigning start position
+    		return substr($item, $substrStart, $endPos - $substrStart);	// Returning string between start and end positions
+    	}
     }
 ?>
