de5f0bf468d66e40d8697b604128aee228ba1f0b
Jacob Ward <jacobward@Jacobs-MacBook-Pro.local>
2014-10-24 11:43:04 +0100

    Fixing indentation

--- stringExtract.php
+++ stringExtract.php
@@ -1,7 +1,7 @@
 <?php
     // Function for extracting a string, given a start and end point
     function stringExtract($item, $start, $end){
-	    $item = stristr($item, $start);
+        $item = stristr($item, $start);
     	$item = substr($item, strlen($start));
     	$stop = stripos($item, $end);
     	$data = substr($item, 0, $stop);
