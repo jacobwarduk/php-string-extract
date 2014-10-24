f9b5bb5487a86a6057bddd7f767845507580c832
Jacob Ward <jacobward@Jacobs-MacBook-Pro.local>
2014-10-24 11:37:54 +0100

    PHP String Extract
    Extract a string given a start and end point.

---
+++ stringExtract.php
@@ -0,0 +1,10 @@
+<?php
+    // Function for extracting a string, given a start and end point
+    function stringExtract($item, $start, $end){
+	    $item = stristr($item, $start);
+    	$item = substr($item, strlen($start));
+    	$stop = stripos($item, $end);
+    	$data = substr($item, 0, $stop);
+    	return $data;
+    }
+?>
